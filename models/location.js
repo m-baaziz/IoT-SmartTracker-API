d830c978ffb76d38d35aa3e2c6fb230c9cb38457
m-baaziz <mha.baaziz@gmail.com>
2016-11-30 21:10:29 +0100

    location post and get

--- models/location.js
+++ models/location.js
@@ -1,10 +1,12 @@
-import { Schema, model } from 'mongoose'
+import mongoose, { Schema } from 'mongoose'
 
 const locationSchema = new Schema({
 	collectedAt: Date,
 	latitude: Number,
 	longitude: Number,
 	accuracy: Number
+}, {
+	timestamps: true
 });
 
-module.exports = model('Location', locationSchema);
+module.exports = mongoose.model('Location', locationSchema);
--- server.js
+++ server.js
@@ -8,13 +8,43 @@ import Location from './models/location'
 const app = express();
 const router = express.Router();
 
+app.use(bodyParser.urlencoded({
+		extended: true
+	}))
+	.use(morgan('combined'))
+	.use('/api', router);
+
 mongoose.connect('mongodb://localhost:27017/smarttracker');
 
-app.use(morgan('combined'))
-	.use('/api', router);
+// GET 
 
 router.get('/devices', (req, res) => {
 	res.json({devices: ["Device1", "Device2", "BlaBla"]});
+})
+	.get('/locations', (req, res) => {
+		Location.find((error, locations) => {
+			if (error) res.send(error);
+
+			res.json({ locations });
+		})
+	})
+
+// POST
+
+router.post('/location', (req, res) => {
+	const { collectedAt, latitude, longitude, accuracy } = req.body;
+	console.log(req.body)
+	const location = new Location();
+	location.collectedAt = collectedAt;
+	location.latitude = latitude;
+	location.longitude = longitude;
+	location.accuracy = accuracy;
+
+	location.save(error => {
+		if (error) res.send(error);
+
+		res.json({ message: "Location successfully added", location });
+	});
 })
 	
 
