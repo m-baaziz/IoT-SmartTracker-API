651b5e6f4b6cd2e7a69a518d490dd532be865f54
m-baaziz <mha.baaziz@gmail.com>
2016-12-07 21:43:15 +0100

    fix scan formating for google api

--- controllers/locationsController.js
+++ controllers/locationsController.js
@@ -16,12 +16,16 @@ const post = (req, res) => {
 	const { deviceMac, deviceIpv4, collectedAt, scanResult } = req.body;
 	const location = new Location();
 	location.collectedAt = collectedAt;
+	const wifiAccessPoints = _.map(scanResult, i => { 
+		const { macAddress, signalStrength } = i
+		return {  macAddress, signalStrength };
+	})
 
 	const deviceCallback = (error, device) => {
 		if (error) res.send(error);
 
 		request.post( 'https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyB8V9-S8-wbtWjY0OjJY8PApKEeb9fiTI0',
-		  { json: { "wifiAccessPoints": _.map(scanResult, ["macAddress, signalStrength"]) } },
+		  { json: { wifiAccessPoints } },
 	    (error, response, body) => {
 	    	if (error) res.send(error);
         if (!error && response.statusCode == 200) {
@@ -39,7 +43,13 @@ const post = (req, res) => {
 
 							const deviceUserSocket = req.app.get('sockets')[device.user];
 							if (deviceUserSocket) {
-								deviceUserSocket.emit('newLocation', { mac: device.mac, latitude: location.latitude, longitude: location.longitude, accuracy: location.accuracy });
+								deviceUserSocket.emit('newLocation', {
+								 mac: device.mac,
+								 latitude: location.latitude,
+								 longitude: location.longitude, 
+								 accuracy: location.accuracy,
+								 collectedAt: location.collectedAt
+								});
 							}
 
 							res.json({ message: `Location successfully added to device ${device._id}`, location });
